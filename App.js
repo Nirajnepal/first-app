58e4971eb94fba5a047cadce9e8ff8e9c4df738a
Prajjwal <bhandariprajjwal123@gmail.com>
2022-09-16 13:58:20 -0400

    First App

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+export default function MainApp(){
+  return(
+    <View style={{
+      flex:1,
+      justifyContent: 'center',
+      alignItems: 'center'
+    }}>
+      <Text>Niraj Nepal</Text>
+      <Text>301211100</Text>
+    </View>
+    
+  )
+}
